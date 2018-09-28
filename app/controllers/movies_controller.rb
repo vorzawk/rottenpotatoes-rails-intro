c7f2f3f178e079a35947aec83069f7eda69d576d
EC2 Default User <ec2-user@ip-172-31-35-154.us-east-2.compute.internal>
2018-09-28 18:51:18 +0000

    part 2 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,13 +11,31 @@ class MoviesController < ApplicationController
   end
 
   def index
+    # The goal here is to execute the database query and provide the Movies
+    # ActiveRecord to the view.
+    # Initialize the @movies variable with all the movies.
     @movies = Movie.all
+    @all_ratings = Movie.distinct.pluck("rating")
+    # All boxes must be checked intially and user choice must not be forgotten 
+    # while displaying the movie list again. This is done by using the checked
+    # argument in the check_box_tag function in the view.
+    @checked = {}
+    @all_ratings.each {|rating|
+        @checked[rating] = true
+    }
+    # Overwrite the @movies variable with the appropriate values depending on the 
+    # values in params
     if params.has_key?(:sort_by) 
        if params[:sort_by] == 'movie_title'
         @movies = Movie.order("title")
        elsif params[:sort_by] == 'release_date'
         @movies = Movie.order("release_date")
        end
+    end
+    if params.has_key?(:ratings)
+        ratings = params[:ratings].keys
+        @movies = Movie.where(rating: ratings)
+        @checked = params[:ratings]
     end
   end
 
